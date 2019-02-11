def452797be431b67e4c36cf213c32535ce37ca4
estigum <estigum@gmail.com>
2019-02-11 11:11:51 -0500

    Reworked DbWriter
    Made it a class and it looks better

--- DbWriter.py
+++ DbWriter.py
@@ -1,47 +1,71 @@
-import threading, logging, time
 from kafka import KafkaConsumer
-import binascii
 import mysql.connector
+import sys
 
 
 class DBWriter(object):
 
     def __init__(self, host, user, password):
-def publishUpdate(mydb, message):
-    temp = message.split(',')
-    mycursor = mydb.cursor()
-    bid = float(temp[2])
-    offer = float(temp[3])
-    sqlString ="update cryptotopofbook set sequenceno=%s,bestbid=%.2f,bestoffer=%.2f where symbol='%s'" % (temp[0],bid,offer, temp[1])
-    mycursor.execute(sqlString)
-    mydb.commit()
+
+        self.Host = host
+        self.User = user
+        self.Password = password
+        self.Db = None
+
+    def connectToDb(self):
+        self.Db = mysql.connector.connect(
+            host = self.Host,
+            user = self.User,
+            passwd = self.Password
+        )
+        print(self.Db)
+
+    def changeDb(self, dbname):
+        mycursor = self.Db.cursor()
+        mycursor.execute("use " + dbname)
+        self.Db.commit()
+
+    def getKafkaConsumer(self):
+
+        consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
+
+                                 auto_offset_reset='earliest',
+
+                                 consumer_timeout_ms=1000)
+        return consumer
+
+    def publishUpdate(self, message):
+        temp = message.split(',')
+        mycursor = self.Db.cursor()
+        bid = float(temp[2])
+        offer = float(temp[3])
+        sqlString ="update cryptotopofbook set sequenceno=%s,bestbid=%.2f,bestoffer=%.2f where symbol='%s'" % (temp[0],bid,offer, temp[1])
+        mycursor.execute(sqlString)
+        self.Db.commit()
+
+    def run(self, database):
+        self.connectToDb()
+        self.changeDb(database)
+        consumer = self.getKafkaConsumer()
+
+        consumer.subscribe(['GDAXFeed'])
+
+        while 1:
+            for message in consumer:
+                msg = message[6].decode("utf-8")
+                if "," in msg:
+                    print(msg)
+                    self.publishUpdate(msg)
+
 
 def main():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="Test"
-    )
 
-    print(mydb)
-    mycursor = mydb.cursor()
-    mycursor.execute("use StockMarketGame")
-    mydb.commit()
+    if len(sys.argv) < 5:
+        print("usage: DbWriter <host> <user> <password> <database>")
+        exit(1)
 
-    consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
-
-                             auto_offset_reset='earliest',
-
-                             consumer_timeout_ms=1000)
-
-    consumer.subscribe(['GDAXFeed'])
-    val = 1
-    while val == 1:
-        for message in consumer:
-            msg = message[6].decode("utf-8")
-            if "," in msg:
-                print(msg)
-                publishUpdate(mydb, msg)
+    writer = DBWriter(sys.argv[1], sys.argv[2], sys.argv[3])
+    writer.run(sys.argv[4])
 
 
 if __name__ == '__main__':
