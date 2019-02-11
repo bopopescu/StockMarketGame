caeb9b9082bc02fd08eb12cbcf76c388e87356dd
estigum <estigum@gmail.com>
2019-02-11 15:54:45 -0500

    Only process missed messages
    Now looking at sequence for a given ticker to see if we have processed it

--- DbWriter.py
+++ DbWriter.py
@@ -11,6 +11,7 @@ class DBWriter(object):
         self.User = user
         self.Password = password
         self.Db = None
+        self.StartSeq = {}
 
     def connectToDb(self):
         self.Db = mysql.connector.connect(
@@ -36,12 +37,30 @@ class DBWriter(object):
 
     def publishUpdate(self, message):
         temp = message.split(',')
+        seq = int(temp[0])
+        symbol = temp[1]
+        if(seq <= self.StartSeq[symbol]):
+            return
+
         mycursor = self.Db.cursor()
         bid = float(temp[2])
         offer = float(temp[3])
         sqlString ="update cryptotopofbook set sequenceno=%s,bestbid=%.2f,bestoffer=%.2f where symbol='%s'" % (temp[0],bid,offer, temp[1])
         mycursor.execute(sqlString)
         self.Db.commit()
+
+        print(message)
+
+    def getStartSequences(self):
+
+        sqlString = "select symbol, sequenceno from cryptotopofbook"
+        mycursor = self.Db.cursor();
+        mycursor.execute(sqlString)
+        results = mycursor.fetchall()
+
+        for result in results:
+            self.StartSeq[result[0]] = int(result[1])
+
 
     def run(self, database):
         self.connectToDb()
@@ -50,11 +69,12 @@ class DBWriter(object):
 
         consumer.subscribe(['GDAXFeed'])
 
+        self.getStartSequences()
+
         while 1:
             for message in consumer:
                 msg = message[6].decode("utf-8")
                 if "," in msg:
-                    print(msg)
                     self.publishUpdate(msg)
 
 
