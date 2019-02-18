328e43a325fdee564061fffc0d3919eda6042fe7
estigum <estigum@gmail.com>
2019-02-18 16:07:39 -0500

    Have one file contains all tables
    Put all table creates into one file.  At top it will truncate and then drop the table

---
+++ Schemas/SMGTables.sql
@@ -0,0 +1,52 @@
+TRUNCATE TABLE cryptotopofbook;
+DROP TABLE cryptotopofbook;
+
+TRUNCATE TABLE smgfill;
+DROP TABLE smgfill;
+
+TRUNCATE TABLE smgorder;
+DROP TABLE smgorder;
+
+CREATE TABLE cryptotopofbook
+(
+    sequenceno bigint,
+    symbol varchar(30) NOT NULL,
+    bestbid decimal(18,8),
+    bestoffer decimal(18,8),
+    timestamp datetime,
+    PRIMARY KEY (symbol)
+);
+
+CREATE TABLE smgfill
+(
+	ordersystem varchar(50),
+	orderId varchar(40) not NULL,
+	fillId varchar(40) not NULL,
+    qty decimal(15,6),
+    price decimal(18,8),
+	created datetime,
+	refId varchar(40),
+    PRIMARY KEY (orderId,fillId)
+);
+
+CREATE TABLE smgorder
+(
+	ordersystem varchar(50),
+	orderId varchar(40) not NULL,
+    parentId varchar(40),
+    symbol varchar(20),
+    side varchar(10),
+    qty decimal(15,6),
+    doneqty decimal(15,6),
+    openqty decimal(15,6),
+    price decimal(18,8),
+	limitprice decimal(18,8),
+	created datetime,
+    lastupdate datetime,
+    ordtype int,
+    tif varchar(10),
+    state int,
+    extorderid varchar(40),
+    extsystem varchar(50),
+    PRIMARY KEY (orderid)
+);
