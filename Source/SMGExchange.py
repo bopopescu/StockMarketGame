77c71bd4cde7fd9b6b940233378fe43270828575
estigum <estigum@gmail.com>
2019-02-19 21:18:35 -0500

    Recovery on new orders processed
    Added in ability to check orders coming form other systems to make sure we don't process them twice.

--- Source/SMGExchange.py
+++ Source/SMGExchange.py
@@ -21,6 +21,7 @@ class SMGExchange(object):
         self.Consumer = KafkaConsumer(bootstrap_servers='localhost:9092', auto_offset_reset='earliest', consumer_timeout_ms=1000)
         self.DB = DBOrderManagerWriter(hostName, user, password, dbName)
         self.Logger = SMGLogger(logName, logLevel)
+        self.RecOrderIds = {}
 
     def setFillSeq(self):
 
@@ -74,6 +75,26 @@ class SMGExchange(object):
 
         self.OM.setOrderSeq(int(temp[1]))
 
+    def getProcessOrderIdsByStem(self):
+
+        sqlString = "select distinct extsystem from smgorder"
+        results = self.DB.Db.select(sqlString)
+        if len(results) == 0:
+            return
+        for result in results:
+            sqlString = "select max(lastupdate) from smgorder where ordersystem = 'SMGExchange' and extsystem ='%s'" % (result[0])
+            lastupdateres = self.DB.Db.select(sqlString)
+            for litem in lastupdateres:
+                sqlString = "select extorderid from smgorder where ordersystem = 'SMGExchange'"\
+                            " and extsystem = '%s' and lastupdate = '%s'" % (result[0], litem[0])
+                extresults = self.DB.Db.select(sqlString)
+                for extorderid in extresults:
+                    temp = extorderid[0].split('-');
+                    if len(temp) != 2:
+                        self.Logger.info("Error splitting external orderId " + extorderid)
+                        continue
+                    self.RecOrderIds[temp[0]] = int(temp[1])
+
     def processBidOffer(self,message):
 
         temp = message.split(',')
@@ -100,6 +121,20 @@ class SMGExchange(object):
 
     def processOrder(self,message):
 
+        temp = message.split(',')
+        if len(temp) != 16:
+            return
+
+        extOrderId = temp[0]
+        etemp = extOrderId.split('-')
+        if len(etemp) !=2:
+            self.Logger.info("Error processing order parsing extOrderId - " + extOrderId)
+            return
+
+        if etemp[0] in self.RecOrderIds:
+            if int(etemp[1]) <= self.RecOrderIds[etemp[0]]:
+                return
+
         order = self.OM.createOrderFromMsg(message)
         self.DB.saveNewOrder(order)
         price = self.getPrice(order.Symbol, order.Side)
@@ -115,6 +150,7 @@ class SMGExchange(object):
 
         self.setFillSeq()
         self.setOrderSeq()
+        self.getProcessOrderIdsByStem()
         self.Logger.info("Subscribing to GDAXFeed and SMGExchangeOrder")
         self.Consumer.subscribe(['GDAXFeed', 'SMGExchangeOrder'])
 
