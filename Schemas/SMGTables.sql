14952788d93737e099f7516a2267aa360d60f923
estigum <estigum@gmail.com>
2019-02-24 09:16:35 -0500

    Adding user tables
    Added user tables around users

--- Schemas/SMGTables.sql
+++ Schemas/SMGTables.sql
@@ -9,6 +9,18 @@ DROP TABLE smgfill IF EXISTS;
 TRUNCATE TABLE smgorder;
 DROP TABLE smgorder IF EXISTS;
 
+TRUNCATE TABLE smguserhistory;
+DROP TABLE smguserhistory;
+
+TRUNCATE TABLE smgposition;
+DROP TABLE smgposition;
+
+TRUNCATE TABLE smgportfolio;
+DROP TABLE smgportfolio
+
+TRUNCATE TABLE smguser;
+DROP TABLE smguser;
+
 CREATE TABLE cryptotopofbook
 (
     sequenceno bigint,
@@ -51,4 +63,43 @@ CREATE TABLE smgorder
     extorderid varchar(40),
     extsystem varchar(50),
     PRIMARY KEY (orderid)
+);
+
+CREATE TABLE smguser
+(
+    userid int AUTO_INCREMENT PRIMARY KEY,
+    username varchar(30),
+    password varchar(30),
+    fullname varchar(50),
+    email varchar(100)
+);
+
+CREATE TABLE smguserhistory
+(
+    userid int NOT NULL,
+    lastupdate datetime,
+    status varchar(20),
+    FOREIGN KEY (userid),
+    REFERENCES smguser(userid)
+);
+
+CREATE TABLE smgportfolio
+(
+    userid int NOT NULL,
+    amount decimal(18,6),
+    created datetime,
+    lastupdate datetime,
+    FOREIGN KEY (userid)
+    REFERENCES smguser (userid)
+);
+
+CREATE TABLE smgposition
+(
+    userid int NOT NULL,
+    symbol varchar(30),
+    amount decimal(18,6),
+    created datetime,
+    lastupdate datetime,
+    FOREIGN KEY (userid)
+    REFERENCES smguser (userid)
 );
